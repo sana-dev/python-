9083fc716dbdaa27f6fa549364016acc8f6fa91d
sana-dev <55602404+sana-dev@users.noreply.github.com>
2021-09-14 13:02:12 +0200

    task01

---
+++ L03.py
@@ -0,0 +1,26 @@
+
+print('hello there')
+
+first_name= 'sana'
+last_name= 'mehak'
+
+
+print(first_name,last_name)
+
+job= input('what do you do for living?')
+
+
+first_number= int(input('what is your first number?'))
+second_number= int(input('what is your second number?'))
+
+sum= first_number+ second_number
+
+print('job'+ job)
+print(first_number,'+' ,second_number,'=',sum)
+ first_number= 3 
+ second_number= 4 
+ thirs_number= 6
+ PI=3.14
+ radis= 8.345
+ circumference= PI*(2 * radius)
+ print(first_number, '\t+\t', second_number, '\t=\t', first_number + second_number)
